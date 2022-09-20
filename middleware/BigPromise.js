0a40c104a72a629fa806a4e2c06dcfea0b237276
Shruti <shrutishinde736@gmail.com>
2022-09-21 01:58:42 +0530

    freelancer and employer models added

--- middleware/BigPromise.js
+++ middleware/BigPromise.js
@@ -3,6 +3,5 @@
 // we've to use try catch along with async await or promise everywhere
 
 module.exports = (func) => (req, res, next) => {
-    Promise.resolve(func(req, res, next)).catch(next);
-  };
-  
+  Promise.resolve(func(req, res, next)).catch(next);
+};
