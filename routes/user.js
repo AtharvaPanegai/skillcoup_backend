90f74f642774171b2b252c10b363ed7f4b8a01dd
AtharvaPanegai <panegai.atharv@gmail.com>
2022-11-28 16:51:47 +0530

    get single user details by id

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { signup, signin, logout, getLoggedInUserDetails, adminAllUsers, admingetSingleUser, adminGetUsersByRole, adminUpdateOneUserDetail, adminDeleteSingleUserById } = require("../controller/userController");
+const { signup, signin, logout, getLoggedInUserDetails, adminAllUsers, admingetSingleUser, adminGetUsersByRole, adminUpdateOneUserDetail, adminDeleteSingleUserById, getUserById } = require("../controller/userController");
 const { isLoggedIn } = require("../middleware/userMiddlewares");
 
 router.route("/signup").post(signup);
 router.route("/signin").post(signin)
 router.route("/signout").get(logout);
 router.route("/whoami").get(isLoggedIn,getLoggedInUserDetails)
+router.route("/getUserDetails").post(isLoggedIn,getUserById);
 
 
 
